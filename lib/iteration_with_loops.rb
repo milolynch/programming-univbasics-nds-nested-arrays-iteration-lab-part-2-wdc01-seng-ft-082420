ba4782381230bc82df34478aaf8813d75fc0490a
beatific-chain-3778 <milobravman20@gmail.com>
2020-08-14 10:57:24 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,16 +1,15 @@
 def find_min_in_nested_arrays(src)
-  
-  new_ary = []
-  row_i = 0
-  while row_i < src.count do
-    ele_i = 0
-    smalv = 100
-    while ele_i < src[row_i].count do
-      if 10 == 9
-        puts what
-      ele_i += 1
+  new_array = []
+  row_index = 0
+  while row_index < src.count do
+    element_index = 0
+    smal_v = 100
+    while element_index < src[row_index].count do
+      if src[row_index][element_index] < smal_v
+        smal_v = src[row_index][element_index]
+      element_index += 1
     end
-    row_i += 1
+    row_index += 1
   end
-  return new_ary
+  return new_array
 end

dae52dda2c2394bde29f5227ce4215135221c557
beatific-chain-3778 <milobravman20@gmail.com>
2020-08-14 10:49:29 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,8 +6,6 @@ def find_min_in_nested_arrays(src)
     ele_i = 0
     smalv = 100
     while ele_i < src[row_i].count do
-      if (src[row_i][ele_i] < smalv)
-        smalv = src[row_i][ele_i]
       ele_i += 1
     end
     row_i += 1
